206bd1a2b3eb546e42fb240a58d44b6f1f8f16da
Nikolay Eskov <mr.eskov1@yandex.ru>
2023-05-19 17:48:43 +0300

    Add some methods for assets snapshot applying.

--- pkg/state/snapshot_applier.go
+++ pkg/state/snapshot_applier.go
@@ -8,6 +8,9 @@ import (
 type snapshotApplier struct {
 	balances *balances
 	aliases  *aliases
+	assets   *assets
+	//scriptsStorage    *scriptsStorage
+	//scriptsComplexity *scriptsComplexity
 }
 
 var _ = (&snapshotApplier{}).applyWavesBalance // TODO: remove it, need for linter for now
@@ -58,3 +61,52 @@ var _ = (&snapshotApplier{}).applyAlias // TODO: remove it, need for linter for 
 func (a *snapshotApplier) applyAlias(blockID proto.BlockID, snapshot AliasSnapshot) error {
 	return a.aliases.createAlias(snapshot.Alias.Alias, snapshot.Address, blockID)
 }
+
+var _ = (&snapshotApplier{}).applyStaticAssetInfo // TODO: remove it, need for linter for now
+
+func (a *snapshotApplier) applyStaticAssetInfo(blockID proto.BlockID, snapshot StaticAssetInfoSnapshot) error {
+	assetID := proto.AssetIDFromDigest(snapshot.AssetID)
+	info := &assetInfo{
+		assetConstInfo: assetConstInfo{
+			tail:                 proto.DigestTail(snapshot.AssetID),
+			issuer:               snapshot.IssuerPublicKey,
+			decimals:             snapshot.Decimals,
+			issueHeight:          0, // TODO: add info?
+			issueSequenceInBlock: 0, // TODO: add info?
+		},
+		assetChangeableInfo: assetChangeableInfo{}, // TODO: add info?
+	}
+	return a.assets.issueAsset(assetID, info, blockID)
+}
+
+var _ = (&snapshotApplier{}).applyAssetDescription // TODO: remove it, need for linter for now
+
+func (a *snapshotApplier) applyAssetDescription(blockID proto.BlockID, snapshot AssetDescriptionSnapshot) error {
+	change := &assetInfoChange{
+		newName:        snapshot.AssetName,
+		newDescription: snapshot.AssetDescription,
+		newHeight:      snapshot.ChangeHeight,
+	}
+	return a.assets.updateAssetInfo(snapshot.AssetID, change, blockID)
+}
+
+var _ = (&snapshotApplier{}).applyAssetVolume // TODO: remove it, need for linter for now
+
+func (a *snapshotApplier) applyAssetVolume(blockID proto.BlockID, snapshot AssetVolumeSnapshot) error {
+	assetID := proto.AssetIDFromDigest(snapshot.AssetID)
+	info, err := a.assets.newestAssetInfo(assetID)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get newest asset info for asset %q", snapshot.AssetID.String())
+	}
+	info.assetChangeableInfo.reissuable = snapshot.IsReissuable
+	info.assetChangeableInfo.quantity = snapshot.TotalQuantity
+	return a.assets.storeAssetInfo(assetID, info, blockID)
+}
+
+//func (a *snapshotApplier) applyAssetScript(blockID proto.BlockID, snapshot AssetScriptSnapshot) error {
+//	var pk crypto.PublicKey // TODO: stub for now, can be omitted for assets. any change will cause state incompatibility for now
+//	_ = snapshot.Complexity // this field is not necessary for us because we do parsing for each script
+//	err := a.scriptsStorage.setAssetScript(snapshot.AssetID, snapshot.Script, pk, blockID)
+//
+//	scriptsComplexity.saveComplexitiesForAsset()
+//}
